67ba4a62cdd762ed388913d6f427e3e6cbe046e1
Chris Ward <chriswarddev@gmail.com>
2015-02-09 17:10:50 +0000

    passengers can enter station

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,4 +3,14 @@ class Passenger
 	def board!(coach)
 		true
 	end
+
+	def unboard!(coach)
+		true
+	end
+
+	def enter_station!(station)
+		true
+	end
+
+
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -2,11 +2,22 @@ require 'passenger'
 
 describe Passenger do
 	
-	let(:coach) {double :coach}
 	let(:passenger)	{Passenger.new}
+	let(:coach) {double :coach}
+	let(:station) {double :station}
 
 	it 'should be able to board a coach' do
 		expect(passenger.board!(coach)).to eq true
 	end
+
+	it 'should be able to exit a coach' do
+		expect(passenger.unboard!(coach)).to eq true
+	end
+
+	it 'should be able to enter a station' do
+		expect(passenger.enter_station!(station)).to eq true
+	end
+
+
 
 end
